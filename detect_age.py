f42fa050f7e5823393191a1b6d3036617a81fdbf
poream3387 <poream3387@gmail.com>
2021-01-10 14:21:30 +0900

    Add pretrained model

--- detect_age.py
+++ detect_age.py
@@ -13,3 +13,16 @@ args = vars(ap.parse_args)
 
 AGE_BUCKETS = ["(0-2)", "(4-6)", "(8-12)", "(15-20)", "(25-32)",
 	"(38-43)", "(48-53)", "(60-100)"]
+
+# Load pretrained Face detector model
+print("[INFO] Loading face detector model...")
+face_prototxt_path = os.path.join([args["face"], "deploy.prototxt"])
+face_weights_path = os.path.join([args["face"], "res10_300x300_ssd_iter_140000.caffemodel"])
+face_net = cv2.dnn.readNet(face_prototxt_path, face_weights_path)
+
+# Load pretrained age detection model
+print("[INFO] Loading age detector model...")
+age_prototxt_path = os.path.join([args["age"], "age_deploy.prototxt"])
+age_weights_path = os.path.join([args["age"], "age_net.caffemodel"])
+age_net = cv2.dnn.readNet(age_prototxt_path, age_weights_path)
+
